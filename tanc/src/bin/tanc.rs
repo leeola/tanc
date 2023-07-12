a4e7929fb05476068b842979223beda25ce044c9
Lee Olayvar <leegit@fastmail.com>
2023-07-12 08:22:52 -0700

    feat: support RUST_LOG filtering and -v -q

--- tanc/src/bin/tanc.rs
+++ tanc/src/bin/tanc.rs
@@ -11,7 +11,7 @@ pub struct CliConfig {
     #[arg(long)]
     pub log_file: Option<PathBuf>,
     #[arg(long)]
-    pub dont_append_log: bool,
+    pub trunc_log: bool,
     #[arg(long)]
     pub dont_log_stderr: bool,
 
@@ -36,6 +36,7 @@ pub struct CliConfig {
         short = 'q',
         action = ArgAction::Count,
         global = true,
+        conflicts_with = "verbose",
     )]
     pub quiet: u8,
 }
@@ -43,7 +44,6 @@ pub struct CliConfig {
 #[tokio::main]
 async fn main() -> Result<()> {
     let config = CliConfig::parse();
-    dbg!(&config);
 
     let (log_file, _log_file_guard) =
         config
@@ -53,34 +53,47 @@ async fn main() -> Result<()> {
                 let f = File::options()
                     .create(true)
                     .write(true)
-                    .append(!config.dont_append_log)
+                    // NIT: Not sure if this inversion is required, but the behavior seemed odd
+                    // without this.
+                    .append(!config.trunc_log)
+                    .truncate(config.trunc_log)
                     .open(path)?;
                 let (non_blocking, guard) = tracing_appender::non_blocking(f);
                 let w = Layer::new().with_writer(non_blocking);
                 Ok((Some(w), Some(guard)))
             })?;
     let (log_stderr, _log_stderr_guard) = if !config.dont_log_stderr {
-        let (non_blocking, guard) = tracing_appender::non_blocking(std::io::stdout());
+        let (non_blocking, guard) = tracing_appender::non_blocking(std::io::stderr());
         let stderr = Layer::new().with_writer(non_blocking);
         (Some(stderr), Some(guard))
     } else {
         (None, None)
     };
 
+    let env_filter = EnvFilter::builder()
+        .with_default_directive(
+            match config.verbose as i8 - config.quiet as i8 {
+                i if i <= -2 => LevelFilter::OFF,
+                -1 => LevelFilter::ERROR,
+                0 => LevelFilter::WARN,
+                1 => LevelFilter::INFO,
+                2 => LevelFilter::DEBUG,
+                3 | _ => LevelFilter::TRACE,
+            }
+            .into(),
+        )
+        .with_env_var("RUST_LOG")
+        .from_env_lossy();
+
     subscriber::set_global_default(
-        tracing_subscriber::FmtSubscriber::builder()
-            .with_env_filter(
-                EnvFilter::builder()
-                    .with_default_directive(LevelFilter::INFO.into())
-                    .with_env_var("RUST_LOG")
-                    .from_env_lossy(),
-            )
-            .finish()
+        tracing_subscriber::registry()
+            .with(env_filter)
             .with(log_stderr)
             .with(log_file),
     )
     .unwrap();
 
     tracing::info!("main");
+
     Ok(())
 }
